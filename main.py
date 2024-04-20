b511164455fd7bd954d2d0a6f84fd09e34a88661
FicalyLyashkin <137011075+FicalyLyashkin@users.noreply.github.com>
2024-04-21 01:09:00 +0300

    добавил формы регстрации и входа

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '775664a9b6ace72dedb42f592cb19a2789935126497200fc1aee8eb2a12d23b9'
@@ -20,15 +21,30 @@ def join_room():
     return render_template('join_room.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    return render_template('login.html')
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        print(request.form['email'])
+        print(request.form['password'])
+        print(request.form['auto_entry'])
+        print(request.form['cancel'])
+        print(request.form['register'])
+        return render_template('index.html')
 
-
-@app.route('/registration')
+@app.route('/registration', methods=['POST', 'GET'])
 def registration():
-    return render_template('registration.html')
-
+    if request.method == 'GET':
+        return render_template('registration.html')
+    elif request.method == 'POST':
+        print(request.form['email'])
+        print(request.form['password'])
+        print(request.form['file'])
+        print(request.form['auto_entry'])
+        print(request.form['cancel'])
+        print(request.form['login'])
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
